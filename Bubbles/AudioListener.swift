9594421620607b758ab464c52952668e13af3dbb
Brian Mackey <mackey.brian@gmail.com>
2014-12-08 12:55:48 -0600

    First commit

---
+++ Bubbles/AudioListener.swift
@@ -0,0 +1,16 @@
+//
+//  AudioListener.swift
+//  Bubbles
+//
+//  Created by Brian Mackey on 12/8/14.
+//  Copyright (c) 2014 Brian Mackey. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+public class AudioListeniner{
+    init() {
+        let sharedInstance = AVAudioSession.sharedInstance();
+    }
+}
